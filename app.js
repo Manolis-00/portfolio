1f338c0f19fa066f425b33eec2f7e2ab95dfe188
Manolis Katopis <ekatopis@gmail.com>
2019-01-17 18:02:34 +0200

    added images

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express          = require("express"),
 
 //APP CONFIGURATION
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //επιτρέπει την χρηση των .ejs αρχειων
+app.use(express.static(__dirname + "/public")); //επιτρέπει την πρόσβαση στο public directory
 
 app.get("/", function(req, res){
     res.render("landing");
